5a28ca80c884f50dd2ffbafff0a21f2fa2210199
Jerell James <jerell1998@gmail.com>
2021-03-29 17:32:28 +0100

    assetPreefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
 	pwa: {
 		dest: 'public',
 	},
+	assetPrefix: isProd
+		? 'https://cdn.statically.io/gh/Jerell/next-pwa/main/'
+		: '',
 })

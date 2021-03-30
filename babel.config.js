3845bee718cdfd129fb07ff0182cc86206eab32f
Jerell James <jerell1998@gmail.com>
2021-03-30 13:10:35 +0100

    unit testing setup - babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
 	presets: [
-		'@babel/preset-env',
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					esmodules: true,
+				},
+			},
+		],
 		'next/babel',
 		// '@babel/preset-react',
 		// '@babel/preset-typescript',

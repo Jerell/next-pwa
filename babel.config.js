0a1585e2839fa86d043679bf75d485573143f7a1
Jerell James <jerell1998@gmail.com>
2021-03-30 13:05:44 +0100

    unit test setup

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	presets: [
+		'@babel/preset-env',
+		'next/babel',
+		// '@babel/preset-react',
+		// '@babel/preset-typescript',
+	],
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-syntax-dynamic-import',
+	],
+}
